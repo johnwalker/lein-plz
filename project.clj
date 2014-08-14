8d6485e2b2ef9e360c284b86ed66c23694fd5553
John Walker <john.lou.walker@gmail.com>
2014-08-13 22:08:36 -0700

    +description, +url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-plz "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A leiningen plugin for adding dependencies to projects quickly."
+  :url "http://johnwalker.io/lein-plz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ancient-clj "0.1.9"]
