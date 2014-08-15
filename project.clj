a785db833ac3e1683465272ec32756dfc8531e3a
John Walker <john.lou.walker@gmail.com>
2014-08-14 17:16:22 -0700

    Project page setup

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-plz "0.1.1-SNAPSHOT"
   :description "A Leiningen plugin for adding dependencies to projects quickly."
-  :url "http://johnwalker.io/posts/lein-plz.html"
+  :url "http://johnwalker.io/lein-plz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ancient-clj "0.1.9"]
