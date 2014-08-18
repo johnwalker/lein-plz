ace1a949731b848a2f8ea5e7e081673e0bf6a5e7
John Walker <john.lou.walker@gmail.com>
2014-08-18 16:19:16 -0700

    Upgrade ancient

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-plz "0.1.1"
+(defproject lein-plz "0.1.2-SNAPSHOT"
   :description "A Leiningen plugin for adding dependencies to projects quickly."
   :url "http://johnwalker.io/lein-plz"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[ancient-clj "0.1.9"]
+  :dependencies [[ancient-clj "0.1.10"]
                  [rewrite-clj "0.3.9"]]
   :eval-in-leiningen true)
