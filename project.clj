7368812bbbd04cb3d9cb8f8bfe5f4f9219a793e5
John Walker <john.lou.walker@gmail.com>
2014-08-24 12:22:26 -0700

    Update link to moved project page

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-plz "0.2.1"
   :description "A Leiningen plugin for adding dependencies to projects quickly."
-  :url "http://johnwalker.io/lein-plz"
+  :url "http://johnwalker.io/projects/lein-plz.html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ancient-clj "0.1.10"]
