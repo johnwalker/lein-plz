518218b752020ec34b07cb9ef4922aab94187335
John Walker <john.lou.walker@gmail.com>
2015-02-14 13:42:40 -0500

    v0.3.3
    Derp. Updated metadata.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-plz "0.3.2"
+(defproject lein-plz "0.3.3"
   :description "A Leiningen plugin for adding dependencies to projects quickly."
-  :url "http://johnwalker.io/projects/lein-plz.html"
+  :url "http://johnwalker.io/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[ancient-clj "0.1.10"]
