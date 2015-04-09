f64deba0b1c06dbc393f45bf5d259ca3ce94c967
John L. Walker <john.lou.walker@gmail.com>
2015-04-09 12:16:22 -0400

    Update changelog, release 0.4.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-plz "0.3.4-SNAPSHOT"
+(defproject lein-plz "0.4.0-SNAPSHOT"
   :description "A Leiningen plugin for adding dependencies to projects quickly."
   :url "http://johnwalker.io/"
   :license {:name "Eclipse Public License"
